c2d500d9076886c93f8f76862144891cb995e8bc
Jwlashley <jwlashley@atmo.space>
2025-06-17 19:09:24 -0500

    These buttons too

--- src/components/Discord.vue
+++ src/components/Discord.vue
@@ -4,12 +4,12 @@
     :options="{ threshold: 0.5 }"
     transition="fade-transition"
   >
-    <v-container fluid class="mt-2 pa-16 bg-grey-darken-4">
+    <v-container class="mt-2 pa-16 bg-grey-darken-4" fluid>
       <v-row align="center" justify="center">
         <v-icon color="teal" icon="fa-brands fa-discord" size="x-small" />
         <p class="text-overline ml-2 color">Powered by Discord</p>
       </v-row>
-      <v-row justify="center" align="center">
+      <v-row align="center" justify="center">
         <h2 class="text-h5 font-weight-light text-no-wrap mb-2">
           Join our Discord Community
         </h2>
@@ -21,9 +21,9 @@
           join!
         </p>
       </v-row>
-      <v-row justify="center" class="ga-2 ga-md-8 mt-md-12">
-        <v-btn color="teal-accent-1">Join</v-btn>
-        <v-btn color="orange-accent-1">Become a Supporter</v-btn>
+      <v-row class="ga-2 ga-md-8 mt-md-12" justify="center">
+        <v-btn color="teal-accent-1" href="https://discord.com/invite/HYkKUHXQ8p">Join</v-btn>
+        <v-btn color="orange-accent-1" href="https://discord.com/servers/atmospace-343961922103672834">Become a Supporter</v-btn>
       </v-row>
     </v-container>
   </v-lazy>
